a44fb308a1c28b509f322a7ac8cdba7e76900165
tonyihvn <anthonynwokoma1@gmail.com>
2021-09-09 18:42:51 +0100

    Modified app config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/ministrymanager/'),
+    'url' => env('APP_URL', 'crm.nountutor.com.ng'),
 
     'asset_url' => env('ASSET_URL', null),
 
